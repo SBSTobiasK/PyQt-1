10f058514196f4fceef7f74576cb72773f2bf900
sbstobiask <sbs202223kerchner@gmail.com>
2022-12-20 08:48:36 +0100

    update1

--- pyqttest.py
+++ pyqttest.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtWidgets import QWidget, QApplication, QLabel
 import sys
 
 #Eine Instanz von der Klasse QApplication erzeugen
@@ -9,6 +9,9 @@ window = QWidget()
 window.setWindowTitle("Meine erste PyQt App!")
 window.setGeometry(200, 200, 600, 300)
 
+#Text einfügen
+txt1 = QLabel("Hallo Welt!", parent=window)
+txt1.move(250, 130)
 
 window.show()
 sys.exit(app.exec())
